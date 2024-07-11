966cdaa50eb81bad153fd3a1888be432789584b1
Appnovate <appnovate789@gmail.com>
2024-07-11 12:03:13 +0530

    last

--- config/plugins.js
+++ config/plugins.js
@@ -21,3 +21,57 @@ module.exports = ({ env }) => ({
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "aws-s3",
+//       providerOptions: {
+//         accessKeyId: env("WASABI_ACCESS_KEY"),
+//         secretAccessKey: env("WASABI_SECRET_KEY"),
+//         region: env("WASABI_REGION"),
+//         params: {
+//           ACL: env("WASABI_ACL", "public-read"),
+//           Bucket: env("WASABI_BUCKET"),
+//         },
+//         endpoint: env("WASABI_END_POINT"),
+//       },
+//       actionOptions: {
+//         upload: {
+//           getKey: (file) => `test/${file.hash}${file.ext}`,
+//         },
+//         uploadStream: {
+//           getKey: (file) => `test/${file.hash}${file.ext}`,
+//         },
+//         delete: {},
+//       },
+//     },
+//   },
+// });
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "aws-s3",
+//       providerOptions: {
+//         accessKeyId: env("WASABI_ACCESS_KEY"),
+//         secretAccessKey: env("WASABI_SECRET_KEY"),
+//         region: env("WASABI_REGION"),
+//         params: {
+//           ACL: env("WASABI_ACL", "public-read"),
+//           Bucket: env("WASABI_BUCKET"),
+//         },
+//         endpoint: env("WASABI_END_POINT"),
+//       },
+//       actionOptions: {
+//         upload: {
+//           // Add this to specify the folder
+//           getKey: (file) => `/test/${file.hash}${file.ext}`,
+//         },
+//         uploadStream: {
+//           // Add this to specify the folder
+//           getKey: (file) => `/test/${file.hash}${file.ext}`,
+//         },
+//         delete: {},
+//       },
+//     },
+//   },
+// });
